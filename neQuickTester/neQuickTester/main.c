b189a651b11d607ae1f4b65ffa730ba75cf2fc12
Emets_V <Emets_V@credo-dialogue.com>
2019-11-06 17:49:49 +0300

    fixing math errors and adding verification datasets

--- neQuickTester/neQuickTester/main.c
+++ neQuickTester/neQuickTester/main.c
@@ -1,45 +1,57 @@
-#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
 #include "neQuick.h"
 
-int main()
-{
-    MODIP_st* modip = (MODIP_st*)malloc(sizeof(MODIP_st));
-    GetModipFromFile(modip);
-    CCIR_st* ccir = (CCIR_st*)malloc(sizeof(CCIR_st));
-    GetCCIRFromFile(ccir);
-    double pdKronrodTol[2] = {0.0, 0.0};
-    int siMaxRecurse = 100;
-    double    pdGssPosLLH[3] = {82.49, 297.66 - 360, 78.11};  /*// receiver position  (lat/lon/h)*/               //this one
-    double    pdSatPosLLH[3] = {24.05, -158.03, 20275295.43};  /*// satellite position (lat/lon/h)*/              //this one
-    double    pdSatPosLLH1[3] =  {54.29, 8.23, 20281546.18};
-    int       siMonth = 1;         /*// month during which STEC value is required*/     //this one
-    double    dUT = 0.0;             /*// time (UTC) at which STEC value is required*/    //this one
-    int       siNumCoeff = 3;      /*// number of Az coeffs*/                               //3
-    double    pdCoeff[3] = {236.831641, -0.39362878, 0.00402826613};      /*// Az coeffs*/                                 //these one
-    double    dAzBase = 0.0;         /*// Az value at receiver locations*/
-    NeQuickInputData_st *data = (NeQuickInputData_st*)malloc(sizeof(NeQuickInputData_st));
-    data->pstModip = modip;
-    data->pstCCIR = ccir;
-    data->pdKronrodTol[0] = pdKronrodTol[0];
-    data->pdKronrodTol[1] = pdKronrodTol[1];
-    data->siMaxRecurse = siMaxRecurse;
-    data->pdGssPosLLH[0] = pdGssPosLLH[0];
-    data->pdGssPosLLH[1] = pdGssPosLLH[1];
-    data->pdGssPosLLH[2] = pdGssPosLLH[2];
-    data->pdSatPosLLH[0] = pdSatPosLLH1[0];
-    data->pdSatPosLLH[1] = pdSatPosLLH1[1];
-    data->pdSatPosLLH[2] = pdSatPosLLH1[2];
-    data->siMonth = siMonth;
-    data->dUT = dUT;
-    data->siNumCoeff = siNumCoeff;
-    data->pdCoeff[0] = pdCoeff[0];
-    data->pdCoeff[1] = pdCoeff[1];
-    data->pdCoeff[2] = pdCoeff[2];
-    data->dAzBase = dAzBase;
-    printf("keke\n");
-    /* 3782752979130000.000000 */
-    double res = NeQuick(data) * 40.3 / SQR(1575.42 * 1e6);
-    printf("%f\n", res);
-    
+#include "ccir_f2.h"
+#include "ccir_fm3.h"
+#include "modip.h"
+#include "validation_datasets.h"
+
+void make_test() {
+	const double eps = 3e-2;
+	const int lat = 0;
+	const int lon = 1;
+	const int h = 2;
+	MODIP_st modip = { 0 };
+	modip.pdModip = &gstModip;
+
+	CCIR_st ccir = {0};
+	ccir.pdF2 = &gF2;
+	ccir.pdM3000 = &gFm3;
+	
+	NeQuickInputData_st data = {0};
+	data.siMaxRecurse = 100;
+	data.siNumCoeff = 3;
+	data.pstModip = &modip;
+	data.pstCCIR = &ccir;
+	int datasetId = 0;
+	int rowId = 0;
+	double ans = 0.0;
+	double err = 0.0;
+	for (datasetId = 0; datasetId < 3; datasetId++) {
+		data.pdCoeff[0] = datasets[datasetId].broadcast_coefficients[0];
+		data.pdCoeff[1] = datasets[datasetId].broadcast_coefficients[1];
+		data.pdCoeff[2] = datasets[datasetId].broadcast_coefficients[2];
+		for (rowId = 0; rowId < 36; rowId++) {
+			data.siMonth = datasets[datasetId].inst[rowId].month;
+			data.dUT = datasets[datasetId].inst[rowId].utc;
+			
+			data.pdGssPosLLH[lat] = datasets[datasetId].inst[rowId].stlat;
+			data.pdGssPosLLH[lon] = datasets[datasetId].inst[rowId].stlon;
+			data.pdGssPosLLH[h] = datasets[datasetId].inst[rowId].stheight;
+			
+			data.pdSatPosLLH[lat] = datasets[datasetId].inst[rowId].satlat;
+			data.pdSatPosLLH[lon] = datasets[datasetId].inst[rowId].satlon;
+			data.pdSatPosLLH[h] = datasets[datasetId].inst[rowId].satheight;
+			ans = NeQuick(&data) / 1e16;
+			err = fabs(ans - datasets[datasetId].inst[rowId].stec) / datasets[datasetId].inst[rowId].stec;
+			assert(err  < eps);
+		}
+	}
+}
+
+int main() {
+	make_test();
     return 0;
 }
